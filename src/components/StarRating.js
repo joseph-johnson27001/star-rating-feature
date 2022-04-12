7ee1b6b07dbce9c253c5fa5c43484746550b0f3c
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-12 10:48:39 +0700

    Initialize state for ratings

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
-import Star from './Star';
+import Star from "./Star";
 
 class StarRating extends Component {
-
   // Initialize a 'rating' state
 
+  state = {
+    rating: 0,
+  };
 
   // Write a function that returns 5 Star components
-
 
   // Write an event handler that updates the rating state.
   // Pass the function to a Star component via props
 
-
   render() {
     return (
-      <ul className="course--stars">
-        {/* Render the Star components */}
-      </ul>
+      <ul className="course--stars">{/* Render the Star components */}</ul>
     );
   }
 }
